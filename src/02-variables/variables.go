9f3aa3664d0e23b0543274ff6f732df89f21ca37
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-10-02 23:07:03 -0600

    Primer vistaso a las variables en go

--- src/02-variables/variables.go
+++ src/02-variables/variables.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"rsc.io/quote"
+)
 
 func main() {
-	fmt.Printf("Hola perras")
+	fmt.Println("Hola mundo")
+	fmt.Println(quote.Go())
+
+	// Declaración de variables
+
+	/*Forma uno de declarar variables*/
+	// var firstName, lastName string
+	// var age int
+
+	/*Forma dos de declarar variables*/
+	// Si inciamos una variable en su declaración no es necesario especificar el tipo de dato
+	// var firstName, lastName, age = "Uriel", "Galindo", 20
+
+	//Declaración e inicialisación en linea solo se pueden declarar dentro de funciones
+	firstName, lastName, age := "Uriel", "Galindo", 20
+
+	fmt.Println(firstName, lastName, age)
 }
